2681a2a715e49247809bb31d77a415d43eb58d93
Derekdc30 <derekchoy30@gmail.com>
2025-04-24 00:35:05 +0800

    update model

--- db/models/response.go
+++ db/models/response.go
@@ -9,6 +9,7 @@ import (
 type Response struct {
 	ID          primitive.ObjectID `bson:"_id,omitempty"`
 	SurveyID    primitive.ObjectID `bson:"survey_id" validate:"required"`
+	SurveyToken string             `json:"surveyToken" bson:"surveyToken" validate:"required"`
 	Answers     []Answer           `bson:"answers" validate:"required"`
 	SubmittedAt time.Time          `bson:"submitted_at"`
 }
